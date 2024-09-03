b2fcc4930edee6c0a751b738a106cdc421d04a7d
Jeremy Guerra <jeremy_guerra1@baylor.edu>
2024-09-03 15:14:46 -0500

    fixed incorrect code

--- script1.py
+++ script1.py
@@ -2,28 +2,23 @@
 # Correc the mistakes and produce the following output:
 # Johnny, the total of your purchase is $1,688.70
 
-def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
-
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+price = 1560
+thetax = .0825
 
-calcTotal(thetax,price,name)
+def calcTotal(price, thetax, myname):
+    mytotal = price * (1 + thetax)
+    print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
 
-
-
-
+calcTotal(price, thetax, myname)
 
 ####  PROBLEM 2  ####
 # print out ONLY the integers in the list below #
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
-    print(i)
-
-
+    if type(i) == int:
+        print(i)
 
 
 #### PROBLEM 3 ####
